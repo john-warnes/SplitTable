803b0be4b739823ed5b4245606363c95e58d47aa
John Warnes <johnw@gurutechnologies.net>
2020-09-17 16:28:55 -0600

    Add support for multiable table classes.

--- splitTable.js
+++ splitTable.js
@@ -1,9 +1,23 @@
+
 /**
  * @name Split Tables
- * @description Splits Tables with the Class `splitForPrint`
+ * @description Splits Tables with the given class names
  * @return {Node}
  */
 function splitTables() {
+  splitTable("splitForPrint");
+  splitTable("SecondSpliting");
+}
+
+/**
+ * @name Split Table
+ * @description Splits Tables with the Class `classname`
+ * @return {Node}
+ */
+function splitTable(classname) {
+  if (!classname) alert("Call splitTable with classname of tables you want to spit")
+  console.log(`Spliting: "${classname}"`);
+
   /**
    * @param {String} HTML representing any one element with any number of children
    * @return {Node}
@@ -20,11 +34,15 @@ function splitTables() {
   var _PageNo = 1;
 
   // Save ordinal header for copying to the new tables
-  var thead = document.querySelector("table.splitForPrint > thead");
-  thead.querySelectorAll("thead > tr > th").forEach((th) => (th.style.width = `${th.offsetWidth + 20}px`));
+  console.log(`Saving header`);
+  var thead = document.querySelector(`table.${classname} > thead`);
 
-  document.querySelectorAll("table.splitForPrint > tbody > tr").forEach(function (e) {
+  console.log(`Setting saved header widths`);
+  thead.querySelectorAll(`table.${classname} > thead > tr > th`).forEach((th) => (th.style.width = `${th.offsetWidth + 20}px`));
+
+  document.querySelectorAll(`table.${classname} > tbody > tr`).forEach(function (e) {
     if (_RunningHeight + e.offsetHeight > MaxHeight) {
+      console.log(`Detecting needed split(s): ${_PageNo}`);
       _RunningHeight = 0;
       _PageNo++;
     }
@@ -33,10 +51,11 @@ function splitTables() {
   });
 
   for (i = 1; i <= _PageNo; i++) {
-    document.querySelector("table.splitForPrint").parentElement.appendChild(
+    console.log(`Creating new table: ${i}`);
+    document.querySelector(`table.${classname}`).parentElement.appendChild(
       htmlToElement(
         `<div class="tablePage">
-            <table id="Table${i}" class="splitTable">
+            <table id="newtable${i}" class="${classname}_child splitTable">
               ${thead.outerHTML}
               <tbody></tbody>
             </table>
@@ -44,10 +63,14 @@ function splitTables() {
       )
     );
 
-    var newTable = document.querySelector(`#Table${i} > tbody`);
-    document.querySelectorAll(`table tr[data-page-no="${i}"]`).forEach((r) => newTable.appendChild(r));
+    console.log(`Copy to new table : ${i}`);
+    var newTable = document.querySelector(`#newtable${i}.${classname}_child > tbody`);
+    document.querySelectorAll(`table.${classname} tr[data-page-no="${i}"]`).forEach((r) => newTable.appendChild(r));
   }
-  document.querySelector("table.splitForPrint").remove();
+  console.log(`Removing old talbe: "${classname}"`);
+  document.querySelector(`table.${classname}`).remove();
+  
+  console.log(`Done with spliting: "${classname}"`);
 }
 
 // window.attachEvent("onload", splitTables);
