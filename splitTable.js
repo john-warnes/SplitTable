802fe39281ef4db9031458ab389e9f07b53853e3
John Warnes <johnw@gurutechnologies.net>
2020-10-07 11:35:25 -0600

    WIP inplace table split

--- splitTable.js
+++ splitTable.js
@@ -1,22 +1,14 @@
 
 /**
- * @name Split Tables
- * @description Splits Tables with the given class names
- * @return {Node}
- */
-function splitTables() {
-  splitTable("splitForPrint");
-  splitTable("SecondSplitting");
-}
-
-/**
  * @name Split Table
  * @description Splits Tables with the Class `className`
  * @return {Node}
  */
-function splitTable(className) {
-  if (!className) alert("Call splitTable with className of tables you want to spit")
-  console.log(`Splitting: "${className}"`);
+function splitTables() {
+  let log = console.log;
+  const className = 'splitForPrint';
+  const MaxHeight = 200;
+  log(`Splitting.`);
 
   /**
    * @param {String} HTML representing any one element with any number of children
@@ -28,49 +20,57 @@ function splitTable(className) {
     return template.content.firstChild;
   }
 
-  const MaxHeight = 500;
 
-  var _RunningHeight = 0;
-  var _PageNo = 1;
+  let tables = document.querySelectorAll(`table.${className}`)
 
-  // Save ordinal header for copying to the new tables
-  console.log(`Saving header`);
-  var thead = document.querySelector(`table.${className} > thead`);
+  for (table_index = 0; table_index < tables.length; table_index++) {
+    log(`number of tables: ${tables.length}`)
+    log(`index: ${table_index}`)
 
-  console.log(`Setting saved header widths`);
-  thead.querySelectorAll(`table.${className} > thead > tr > th`).forEach((th) => (th.style.width = `${th.offsetWidth + 20}px`));
+    let ntable = tables[table_index];
 
-  document.querySelectorAll(`table.${className} > tbody > tr`).forEach(function (e) {
-    if (_RunningHeight + e.offsetHeight > MaxHeight) {
-      console.log(`Detecting needed split(s): ${_PageNo}`);
-      _RunningHeight = 0;
-      _PageNo++;
+    let _RunningHeight = 0;
+    let _PageNo = 1;
+
+    // Save ordinal header for copying to the new tables
+    log(`Saving header`);
+    var thead = ntable.querySelector(`thead`);
+
+    log(`Setting saved header widths`);
+    thead.querySelectorAll(`tr > th`).forEach((th) => (th.style.width = `${th.offsetWidth + 20}px`));
+
+    ntable.querySelectorAll(`tbody > tr`).forEach(function (e) {
+      if (_RunningHeight + e.offsetHeight > MaxHeight) {
+        log(`Detecting needed split(s): ${_PageNo}`);
+        _RunningHeight = 0;
+        _PageNo++;
+      }
+      _RunningHeight += e.offsetHeight;
+      e.setAttribute("data-page-no", _PageNo);
+    });
+
+    for (i = 1; i <= _PageNo; i++) {
+      log(`Creating new table: ${i}`);
+      ntable.parentElement.appendChild(
+        htmlToElement(
+          `<div class="tablePage">
+              <table id="${className}_${table_index}_${i}" class="splitTable">
+                ${thead.outerHTML}
+                <tbody></tbody>
+              </table>
+            </div>`
+        )
+      );
+
+      log(`Copying rows to new table : ${i}`);
+      var newTable = document.querySelector(`#${className}_${table_index}_${i}.splitTable > tbody`);
+      ntable.querySelectorAll(`tr[data-page-no="${i}"]`).forEach((r) => newTable.appendChild(r));
     }
-    _RunningHeight += e.offsetHeight;
-    e.setAttribute("data-page-no", _PageNo);
-  });
-
-  for (i = 1; i <= _PageNo; i++) {
-    console.log(`Creating new table: ${i}`);
-    document.querySelector(`table.${className}`).parentElement.appendChild(
-      htmlToElement(
-        `<div class="tablePage">
-            <table id="new_table${i}" class="${className}_child splitTable">
-              ${thead.outerHTML}
-              <tbody></tbody>
-            </table>
-          </div>`
-      )
-    );
-
-    console.log(`Copy to new table : ${i}`);
-    var newTable = document.querySelector(`#new_table${i}.${className}_child > tbody`);
-    document.querySelectorAll(`table.${className} tr[data-page-no="${i}"]`).forEach((r) => newTable.appendChild(r));
+    log(`Removing old table.`);
+    ntable.remove();
   }
-  console.log(`Removing old table: "${className}"`);
-  document.querySelector(`table.${className}`).remove();
-  
-  console.log(`Done with splitting: "${className}"`);
+  log(`Done with splitting.`);
+
 }
 
 // window.attachEvent("onload", splitTables);
